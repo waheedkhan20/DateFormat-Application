d86e690943e9d255d97a013120ed1c329f8d4159
Waheekhan <MohammedAbdulWaheed.Khan@ust.com>
2022-04-21 17:32:33 +0530

    Validation code added

--- DataApplication/Program.cs
+++ DataApplication/Program.cs
@@ -11,31 +11,47 @@ namespace DataApplication
         static void Main(string[] args)
         {
 			Console.WriteLine("Enter Date dd/mm/yyyy Format");
-			int d = Convert.ToInt32(Console.ReadLine());
-			int m = Convert.ToInt32(Console.ReadLine());
-			int y = Convert.ToInt32(Console.ReadLine());
-
-			if (d > 0 && d < 28)    //checking for day from 0 to 27
-				d += 1;
-			if (d == 28)
+			
+			try
 			{
-				if (m == 2) //checking for february
-				{
-					if ((y % 400 == 0) || (y % 100 != 0 || y % 4 == 0)) //leap year check in case of feb
-					{
-						d = 29;
-					}
-					else
-					{
-						d = 1;
-						m = 3;
-					}
-				}
-				else    //when its not feb
+				string dateval = Console.ReadLine();
+				string[] arr = dateval.Split('/');
+				int day = Convert.ToInt32(arr[0]);
+				int d = Convert.ToInt32(day);
+				d = d + 1;
+				arr[0] = Convert.ToString(d);
+				Console.WriteLine(arr[0] + "/" + arr[1] + "/" + arr[2]);
+				int m = Convert.ToInt32(Console.ReadLine());
+				int y = Convert.ToInt32(Console.ReadLine());
+
+				if (d > 0 && d < 28)    //checking for day from 0 to 27
 					d += 1;
+				if (d == 28)
+				{
+					if (m == 2) //checking for february
+					{
+						if ((y % 400 == 0) || (y % 100 != 0 || y % 4 == 0)) //leap year check in case of feb
+						{
+							d = 29;
+						}
+						else
+						{
+							d = 1;
+							m = 3;
+						}
+					}
+					else    //when its not feb
+						d += 1;
+				}
+
+				Console.WriteLine("New date:{0}/{1}/{2}", d, m, y);
+				Console.ReadLine();
 			}
-			Console.WriteLine("New date:{0}/{1}/{2}", d, m, y);
-			Console.ReadLine();
+			catch (Exception ex)
+			{
+				Console.WriteLine("Please enter values in dd/mm/yyyy format");
+
+			}
 		}
     }
 }
